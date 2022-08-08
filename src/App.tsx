ee00196b49a2cee60eb406e633e5dfbd8fc523e9
cansaye <877848417@qq.com>
2022-08-08 17:35:15 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title></Title>
+      <div className="todoBox">
+        <Input></Input>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+        <TodoButtom></TodoButtom>
+      </div>
+    </div>
+  );
+}
+
+function Title() {
+  return <div className="title">todos</div>;
+}
+
+// function TodoBox() {
+//   return <div className="todo-box">1111</div>;
+// }
+
+function Input() {
+  return (
+    <div className="todoInput">
+      <div className="inputIcon"></div>
+      <input type="text" placeholder="What needs to be done?"></input>
+    </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <div className="todoItem">
+      <div className="todoTick"></div>
+      <div className="todoName">Something to do</div>
+    </div>
+  );
+}
+
+function TodoButtom() {
+  return (
+    <div className="todoButtom">
+      <div className="todoLeft">1 item left</div>
+      <div className="todoStatus">
+        <div className="todoAll">All</div>
+        <div className="todoActive">Active</div>
+        <div className="todoCompleted">Completed</div>
+      </div>
+      <div className="todoClear">Clear completed</div>
     </div>
   );
 }

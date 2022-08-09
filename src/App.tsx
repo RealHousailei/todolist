c98ba4e29e9666f34d17d4b5e4c72e929c61d4df
罗曜郁 <877848417@qq.com>
2022-08-09 16:01:51 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 
+interface todoItemProps {
+  ticked?: boolean;
+  name: string;
+}
+
 function App() {
   return (
     <div className="App">
       <Title></Title>
       <div className="todoBox">
         <Input></Input>
-        <TodoItem></TodoItem>
-        <TodoItem></TodoItem>
+        <TodoItem ticked={true} name="something to do"></TodoItem>
+        <TodoItem name="something to do"></TodoItem>
         <TodoButtom></TodoButtom>
       </div>
     </div>
@@ -32,21 +37,45 @@ function Input() {
   );
 }
 
-function TodoItem() {
+const tickedStyle = {
+  color: ' rgb(98, 149, 85, 0.6)',
+  fontSize: '2rem',
+  lineHeight: '3rem',
+  fontFamily: 'Georgia, serif;',
+};
+
+const tickedNameStyle = {
+  textDecoration: 'line-through solid rgb(200, 200, 200)',
+  color: 'rgb(210, 210, 210)',
+};
+
+function TodoItem(props: todoItemProps) {
   return (
     <div className="todoItem">
-      <div className="todoTick"></div>
-      <div className="todoName">Something to do</div>
+      <div className="todoTick" style={props.ticked ? tickedStyle : {}}>
+        {props.ticked ? '✔' : ''}
+      </div>
+      <div className="todoName" style={props.ticked ? tickedNameStyle : {}}>
+        {props.name}
+      </div>
     </div>
   );
 }
+
+const selectedStyle = {
+  border: '2px solid rgb(255, 142, 142)',
+  borderRadius: '10%',
+  padding: '5px 10px',
+};
 
 function TodoButtom() {
   return (
     <div className="todoButtom">
       <div className="todoLeft">1 item left</div>
       <div className="todoStatus">
-        <div className="todoAll">All</div>
+        <div className="todoAll" style={selectedStyle}>
+          All
+        </div>
         <div className="todoActive">Active</div>
         <div className="todoCompleted">Completed</div>
       </div>
